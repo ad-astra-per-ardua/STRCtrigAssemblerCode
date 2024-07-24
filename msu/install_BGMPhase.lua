00a015ca9e1f252135d9459f745236fb3dca26a4
d982h8st7 <sinnarver15@gmail.com>
2024-07-24 17:05:29 +0900

    refactoring

--- msu/install_BGMPhase.lua
+++ msu/install_BGMPhase.lua
@@ -1,15 +1,15 @@
 function Install_BGMPhase()
 Fixedtime = 1000
-BGMVar = CreateVarArr(4,FP)
-OB_BGMVar = CreateVar(FP)
+BGMArray = CreateVarArr(4,FP)
+OB_BGMArray = CreateVar(FP)
     for i = 0, 3 do
-        IBGM_EPD(FP, {i}, BGMVar[i+1], {
+        IBGM_EPD(FP, {i}, BGMArray[i+1], {
         {1, "staredit\\wav\\hat.ogg", 32 * Fixedtime},
         {2, "staredit\\wav\\Lair.ogg", 28 * Fixedtime},
         {3, "staredit\\wav\\hive.ogg", 31 * Fixedtime},
         })
     end
-    IBGM_EPD(FP, {Force5}, OB_BGMVar, {
+    IBGM_EPD(FP, {Force5}, OB_BGMArray, {
         {1, "staredit\\wav\\hat.ogg", 32 * Fixedtime},
         {2, "staredit\\wav\\Lair.ogg", 28 * Fixedtime},
         {3, "staredit\\wav\\hive.ogg", 31 * Fixedtime},
@@ -21,11 +21,11 @@ OB_BGMVar = CreateVar(FP)
         TriggerX(FP,{},{
             CopyCpAction({DisplayTextX(StrDesignX("Hatchery Destroyed! + 30,000 Points"), 4)}, {Force1,Force5}, FP);
             SetScore(Force1, Add, 30000,Kills);
-            SetNVar(BGMVar[1], SetTo, 1);
-            SetNVar(BGMVar[2], SetTo, 1);
-            SetNVar(BGMVar[3], SetTo, 1);
-            SetNVar(BGMVar[4], SetTo, 1);
-            SetNVar(OB_BGMVar, SetTo, 1);
+            SetNVar(BGMArray[1], SetTo, 1);
+            SetNVar(BGMArray[2], SetTo, 1);
+            SetNVar(BGMArray[3], SetTo, 1);
+            SetNVar(BGMArray[4], SetTo, 1);
+            SetNVar(OB_BGMArray, SetTo, 1);
             SetDeaths(P6, SetTo, 0, 131);
         })
     CIfEnd()
